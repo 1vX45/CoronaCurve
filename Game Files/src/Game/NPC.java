1e84fc7a895500c14e8d8c1a8393f6fa733bec87
1vX45 <62425877+1vX45@users.noreply.github.com>
2020-03-21 15:54:08 +0100

    Add files via upload

--- Game Files/src/Game/NPC.java
+++ Game Files/src/Game/NPC.java
@@ -6,10 +6,10 @@ import java.awt.Graphics;
 public class NPC {
 	private int status;
 	private int posX, posY, size;
-	private double speedX, speedY;
+	private int speedX, speedY;
 	private Color color;
 	
-	public NPC (int startX, int startY, int size, double initSpeedX, double initSpeedY, Color initColor){
+	public NPC (int startX, int startY, int size, int initSpeedX, int initSpeedY, Color initColor){
 		status = CoronaCurve.HEALTHY;
 		posX = startX;
 		posY = startY;
@@ -23,10 +23,18 @@ public class NPC {
 		return status;
 	}
 	
+	public int getX(){
+		return posX;
+	}
+	
+	public int getY(){
+		return posY;
+	}
+	
 	/*
 	 *  0: no collision
 	 *  1: collision left or right
-	 *  2: collision up or down
+	 *  2: collision above or below
 	 */
 	public int isBorder(int sizeX, int sizeY){
 		if(posX < 0 || posX > sizeX - size)
@@ -51,6 +59,25 @@ public class NPC {
 			posX += speedX;
 			posY += speedY;
 		}
+	}
+	
+	public void infect(){
+		status = CoronaCurve.INFECTED;
+		color = CoronaCurve.CLR_INFECTED;
+	}
+	
+	public void heal(){
+		status = CoronaCurve.IMMUNE;
+		color = CoronaCurve.CLR_IMMUNE;
+	}
+	
+	public boolean collidesWith(NPC other){
+		int diff_x = Math.abs(posX - other.getX());
+		int diff_y = Math.abs(posY - other.getY());
+		double dist = Math.sqrt(diff_x*diff_x + diff_y*diff_y);
+		if(dist < size) 
+			return true;
+		else return false;
 	}
 	
 	public void paint(Graphics g){
